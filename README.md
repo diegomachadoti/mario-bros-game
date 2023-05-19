50f37f565c51757e45465d074781b491f58c9a17
Diego Paiva Machado <tqi_dmachado@uolinc.com>
2023-05-19 10:19:45 -0300

    Incrementando o README.md v4

--- README.md
+++ README.md
@@ -1,19 +1,24 @@
-# mario-bros-game
+# mario-bros-game ![Badge](https://img.shields.io/badge/JavaScript-323330?style=for-the-badge&logo=javascript&logoColor=F7DF1E) ![Badge](https://img.shields.io/badge/HTML5-E34F26?style=for-the-badge&logo=html5&logoColor=white) ![Badge](https://img.shields.io/badge/CSS3-1572B6?style=for-the-badge&logo=css3&logoColor=white)
 
 ![game-mario.png](game-mario.png)
 **Projeto gamer utilizando HTML, JS e CSS do jogo do Mario Bros.**
 
-
-
 ## **Executar o Game**
 
-Abrir o arquivo **mario-bros-game/index.html** no seu navegador.
-
+-   Abrir o arquivo **mario-bros-game/index.html** no seu navegador.
+-   Outra opção e ativar a extenção Live Server do vscode e start server local acessando atraves do endereço http://127.0.0.1:5500/
 
 ## Itens para evoluir
-- De acordo com o tempo ir acelerando a velocidade
-- Adicionar um score com a pontuação
-- Alternar os objetos pipe adicionando dificuldade
+
+-   De acordo com o tempo ir acelerando a velocidade
+-   Adicionar um score com a pontuação
+-   Alternar os objetos pipe adicionando dificuldade
 
 ## **Links**
-[Repo Imagens](https://drive.google.com/drive/folders/1CYQ2CtPyiXcONexGfpQ1RnaPa30c7PNy)
+
+-   [Repo Imagens](https://drive.google.com/drive/folders/1CYQ2CtPyiXcONexGfpQ1RnaPa30c7PNy)
+-   [Extensão VsCode Live Server](https://github.com/ritwickdey/vscode-live-server-plus-plus)
+-   [Badges README](https://simpleicons.org/)
+
+----
+![YOUR github stats](https://github-readme-stats.vercel.app/api?username=diegomachadoti)
--- js/script.js
+++ js/script.js
@@ -1,71 +1,62 @@
-
 /* pegar o elemento da imagem do mario*/
-const mario = document.querySelector('.mario') 
-const pipe = document.querySelector('.pipe') 
-const gameOver = document.querySelector('.gameOver') 
-
+const mario = document.querySelector(".mario");
+const pipe = document.querySelector(".pipe");
+const gameOver = document.querySelector(".gameOver");
 
 /* Logica para o Mario pular */
 const jump = () => {
-
     /* quando clicar qualquer tecla add a função jump no mario */
-    mario.classList.add('jump') 
-    
-    
+    mario.classList.add("jump");
+
     /* remove a class para ADD de novo conforme timeout da animação mario-jump-animation 500ms para ADD novamente conforme evento do teclado */
-    setTimeout(() =>{
-        mario.classList.remove('jump')
+    setTimeout(() => {
+        mario.classList.remove("jump");
     }, 500);
-
-}
-
+};
 
 /* Logica quando o deslocamento do pipe com o mario encostar o jogo acabe no caso fazer um replace na imagem do mario */
 const loop = setInterval(() => {
-
-    console.log('loop')
+    console.log("loop");
     playMusic();
 
     const pipePosition = pipe.offsetLeft; // delocamento esquerdo
-    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px',''); // trazer a altura e intervalo da posição do mario de acordo com o pulo (+ converte para inteiro)
+    const marioPosition = +window
+        .getComputedStyle(mario)
+        .bottom.replace("px", ""); // trazer a altura e intervalo da posição do mario de acordo com o pulo (+ converte para inteiro)
     //console.log(pipePosition)
     //console.log(marioPosition)
 
     /* Se posicição do pipe com o mario 120 || posição do pipe > 0 || posição do mario com jump ao pipe*/
-    if(pipePosition <= 120 && pipePosition > 0 && marioPosition < 96){
-        pipe.style.animation = 'none'// para animação do pipe
-        pipe.style.left = `${pipePosition}px`  // posiçao de parada exatamento a posição quando encosta no mario
+    if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 96) {
+        pipe.style.animation = "none"; // para animação do pipe
+        pipe.style.left = `${pipePosition}px`; // posiçao de parada exatamento a posição quando encosta no mario
 
-        mario.style.animation = 'none'// para animação do mario
-        mario.style.bottom = `${marioPosition}px`  // posiçao do mario se encostra no pipe seja a mesma do pipe
+        mario.style.animation = "none"; // para animação do mario
+        mario.style.bottom = `${marioPosition}px`; // posiçao do mario se encostra no pipe seja a mesma do pipe
 
         // Se der game over substituir a imagem do mario
-        mario.src = './images/game-over.png'
-        mario.style.width = '75px'
-        mario.style.marginLeft = '50px'
+        mario.src = "./images/game-over.png";
+        mario.style.width = "75px";
+        mario.style.marginLeft = "50px";
 
-        gameOver.src = './images/mario-game-over.png'
-        gameOver.style.display = 'block'; 
+        gameOver.src = "./images/mario-game-over.png";
+        gameOver.style.display = "block";
 
         // para o loop apos gameOver
         clearInterval(loop);
         pauseMusic();
-        
     }
-
-    }, 10)
-
+}, 10);
 
 /* Função para tocar música */
 function playMusic() {
-    document.getElementById('audioContainer').play();
-  }
-
-function pauseMusic() {
-  var audio = document.getElementById('audioContainer');
-  audio.pause();
+    document.getElementById("audioContainer").play();
 }
 
-  
+function pauseMusic() {
+    var audio = document.getElementById("audioContainer");
+    audio.pause();
+}
+
 /* Escutar o evento do teclado */
-document.addEventListener('keydown', jump) 
+document.addEventListener("keydown", jump);
